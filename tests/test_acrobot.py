4728058366a534177878378702ffaba388b2643f
BlankAdventure <>
2026-01-09 16:46:22 -0500

    add tests with event loop active

--- tests/test_acrobot.py
+++ tests/test_acrobot.py
@@ -4,10 +4,10 @@ Created on Fri Sep 19 16:48:10 2025
 
 @author: BlankAdventure
 """
+import time
 import pytest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch, call
 from acrobot.app import match_words, Acrobot
-from acrobot.config import Config
 
 def test_match_words_found():
     message = "Let's grab a beer and go!"
@@ -74,10 +74,67 @@ async def test_add_message_command_updates_history(dummy_bot, mock_update):
     await dummy_bot.command_add_message(mock_update, context)    
     assert dummy_bot.history[-1] == ("Alice", "hello world")
     mock_update.message.reply_text.assert_awaited_once_with("Message added.",do_quote=True)
-
-async def test_fail_build(default_config):
-    del default_config["testconf"]
-    with pytest.raises(KeyError):
-        Acrobot(Config(**default_config),start_telegram=False)
     
+async def test_command_acro_soft_fail(default_config, mock_update, mock_context):
+    
+    default_config['acrobot']['throttle_interval'] = 1
+    default_config['model']['retries'] = 1
+    
+    bot = Acrobot(default_config, start_telegram=False)    
+    
+    with patch.object(bot.llm, 'generate_response') as mock_func:
+
+        mock_func.configure_mock(return_value = "call on weeds")    
+        mock_context.args = ["cow"]    
+        
+        bot.start(run_polling=False) #start bot without polling    
+        await bot.command_acro(mock_update, mock_context)            
+        await bot.complete(stop=False)
+        mock_update.message.reply_text.assert_awaited_once_with('call on weeds', do_quote=False)
+
+        mock_func.configure_mock(return_value = "invalid acronym") 
+        await bot.command_acro(mock_update, mock_context)            
+        await bot.complete(stop=False)
+        mock_update.message.reply_text.assert_awaited_with("invalid acronym", do_quote=False)
+
+        mock_func.configure_mock(side_effect=ValueError) 
+        await bot.command_acro(mock_update, mock_context)            
+        await bot.complete(stop=True)
+        mock_update.message.reply_text.assert_awaited_with('Dammit you broke something', do_quote=True)
+
+        assert len(mock_func.mock_calls) == 5
+        assert len(mock_update.message.reply_text.mock_calls) == 3
+
+
+async def test_command_acro_timing(default_config, mock_update, mock_context):
+    
+    default_config['acrobot']['throttle_interval'] = 2
+    default_config['model']['retries'] = 0
+    
+    bot = Acrobot(default_config, start_telegram=False)    
+    
+    with patch.object(bot.llm, 'generate_response') as mock_func:
+
+        mock_func.configure_mock(return_value = "call on weeds")    
+        mock_context.args = ["cow"]    
+        
+        bot.start(run_polling=False) 
+        
+        start_time = time.perf_counter()
+        
+        await bot.command_acro(mock_update, mock_context)           
+        await bot.command_info(mock_update, mock_context)     
+
+        mock_context.args = ["dog"]    
+        await bot.command_acro(mock_update, mock_context)            
+        await bot.complete(stop=True)
+        
+        duration = time.perf_counter() - start_time        
+    
+    expected = [call('INFO INFO INFO!'),
+                call('call on weeds', do_quote=False),
+                call('call on weeds', do_quote=False)]
+
+    assert mock_update.message.reply_text.mock_calls == expected
+    assert duration == pytest.approx(4.25, abs=0.15)        
     
